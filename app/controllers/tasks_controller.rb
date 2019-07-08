3e7590ec65cae3b8e042c44f4b327979de10670f
satogonia <programmingkota@gmail.com>
2019-07-08 14:55:46 +0000

    CRUD tsks 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,8 +46,9 @@ class TasksController < ApplicationController
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
-end
+
 
   def task_params
     params.require(:task).permit(:content)
   end
+end  
