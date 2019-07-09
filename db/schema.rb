9545964114eca797b03393582c24c6965eb717a0
satogonia <programmingkota@gmail.com>
2019-07-09 07:37:09 +0000

    fix2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_064413) do
+ActiveRecord::Schema.define(version: 2019_07_09_072545) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_064413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "status"
   end
 
 end
